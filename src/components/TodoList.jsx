00b2f25c76696dda5f9e8801cba0108147d773c1
hossein <hossein@yahoo.com>
2025-11-14 23:37:06 +0330

    todo commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,27 @@
-export default function TodoList(){
-    return(
-        <>
+import { useState } from "react"
+
+export default function TodoList() {
+    const [todo, setTodo] = useState([]);
+    const [input, setInput] = useState('');
+
+    const keyDownHandler = (e) => {
+        if (e.key === 'Enter') saveHandler()
+    };
+    const saveHandler = () =>{
         
-        </>
+    }
+    return (
+        <div className="todo">
+            <div>
+                <input
+                    type="text"
+                    placeholder="enter your work pleas ..."
+                    value={input}
+                    onChange={(e) => setTodo(e.target.value)}
+                    onKeyDown={keyDownHandler}
+                />
+                <button onClick={saveHandler}>ثبت</button>
+            </div>
+        </div>
     )
 }
